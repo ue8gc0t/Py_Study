692f581347dc72c8e875593f912c9f2eae0bcb2b
Ue8gc0t_U <ue8gc0tlcn9k6hw8@gmail.com>
2020-10-04 15:58:49 +0300

    New py file Hello world

---
+++ Less_1/Hello_world.py
@@ -0,0 +1,49 @@
+# print("Hello world")
+
+# print(1 + 1)    # суммирование
+# print(10 - 1)   # вычетание
+# print(1 * 1)    # умножение
+# print(10 / 1)   # деление
+# print(2 ** 7)   # возведение в степень
+# print(10 // 3)  # деление без остатка
+# print(10 % 3)  # остаток от деления
+
+# x = 50
+# print(x * 2)
+# x = x * 5
+# x *= 5
+# print(x)
+# speed_auto = 75
+
+# my_var = 'qwe'
+# my_var = 50
+# print(my_var)
+
+# my_int = 5
+# print(type(my_int))
+#
+# my_float = 5.4
+# print(type(my_float))
+
+# a = 5
+# b = '5'
+#
+# print(a + int(b))
+# print(str(a) + b)
+
+# a = 5
+# b = 6
+# c = 10
+# print(a > b)
+# print(a < b)
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a <= b)
+# print((a < b) and (c > b))
+# print((a < b) or (c > b))
+# print(not((a < b) or (c > b)))
+
+# user_answer = int(input("Enter number: "))
+# print(type(user_answer))
+
